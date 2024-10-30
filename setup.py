288fec8248c962901b79ac8dc0cc5e74fed34c29
Ayan Pathan <165584568+ayan2k05@users.noreply.github.com>
2024-10-30 12:31:09 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from cx_Freeze import setup, Executable
 app_name = "OpenEdit"
 app_version = "1.0"
 app_description = r"OpenEdit - Light Editor"
-icon_path = r"C:\Users\Anis Pathan\Desktop\OpenEdit\finalicon.ico"
+#in the below line add the path where finalicon.ico is saved
+icon_path = r"C:\Users\YRepoPath\OpenEdit\finalicon.ico"
 executables = [Executable("OpenEdit.py", base="Win32GUI", icon = icon_path)]
 
 
