95dcbaf5787094c5c84f86113dbe3c780278fcaa
Ayan Pathan <165584568+ayan2k05@users.noreply.github.com>
2024-10-31 15:23:09 +0530

    setup.py
    New Update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ app_name = "OpenEdit"
 app_version = "v1.1"
 app_description = r"OpenEdit - Light Editor"
 #in the below line add the path where finalicon.ico is saved
-icon_path = r"C:\Users\YRepoPath\OpenEdit\finalicon.ico"
+icon_path = r"C:\Users\YRepoPath\OpenEdit\icon.ico"
 executables = [Executable("OpenEdit.py", base="Win32GUI", icon = icon_path)]
 
 
